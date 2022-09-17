6637cf9e6e6f82e4f9a98b8fb97de3e383934427
Wolf-Dieter Klotz <wdklotz@gmail.com>
2022-09-17 08:25:26 +0200

    ...

--- py3_utils/PandaPlotter.py
+++ py3_utils/PandaPlotter.py
@@ -1,23 +1,22 @@
-import sys
-import math
+import sys, os, math, argparse
 import pandas as pd
 # import matplotlib
 # matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import argparse
 
 class PandaPlotter(object):
     def __init__(self,args):
-        self.dir        = args.get('path')
+        self.dir        = args.get('px')
         self.file       = args.get('file')
         self.header     = args.get('nh')
         self.abscissa   = args.get('x')
-        self.ordinate1  = args.get('y1')
+        self.ordinate1  = args.get('y')
         self.ordinate2  = args.get('y2')
         self.ordinate3  = args.get('y3')
         self.ordinate4  = args.get('y4')
     def do_plot(self):
-        qfile_name = f'{self.dir}/{self.file}'
+        qfile_name = f'{self.dir}/{self.file}' if self.dir != "" else f'{self.file}'
+        qfile_name = os.path.normpath(qfile_name)
         print(f'data source: {qfile_name}')
 
         header    = not self.header
@@ -25,7 +24,6 @@ class PandaPlotter(object):
             df = pd.read_csv(qfile_name, sep="\s+")
         else:
             df = pd.read_csv(qfile_name, header=None, sep="\s+")
-        df.info()
 
         x_column  = self.abscissa
         y1_column = self.ordinate1
@@ -40,23 +38,23 @@ class PandaPlotter(object):
         print(f'plotting ordinates "{y_columns}" against abscissa "{x_column}"')
         print('====================================================================')
 
-        df.plot(x_column,y_columns,linestyle='-',linewidth="0.6", title=qfile_name)
-        plt.show()
+        try:
+            df.plot(x_column,y_columns,linestyle='-',linewidth="0.6", title=qfile_name)
+            plt.show()
+        finally:
+            df.info()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # group  = parser.add_mutually_exclusive_group()
-    parser.add_argument ("--path", default="/home/wdklotz/PyOrbit/examples+/SNS_Linac/pyorbit_linac_model", help="path to data source")
-    parser.add_argument ("--file", default="pyorbit_twiss_sizes_ekin.dat", help="data source file")
-    parser.add_argument ("--x",    default="position", help="Abscissa (def = position)")
-    parser.add_argument ("--y1",   default="betaX", help="Ordinate (def = betaX)")
-    parser.add_argument ("--y2",   default="", help="2nd Ordinate")
-    parser.add_argument ("--y3",   default="", help="3rd Ordinate")
-    parser.add_argument ("--y4",   default="", help="4th Ordinate")
+    parser.add_argument ("--px",   default="",          help="prefix to data source")
+    parser.add_argument ("file",   default="data.txt",  help="data source file")
+    parser.add_argument ("x",      default="position",  help="Abscissa")
+    parser.add_argument ("y",      default="betaX",     help="Ordinate")
+    parser.add_argument ("--y2",   default="",          help="2nd Ordinate")
+    parser.add_argument ("--y3",   default="",          help="3rd Ordinate")
+    parser.add_argument ("--y4",   default="",          help="4th Ordinate")
     parser.add_argument ("--nh",   action="store_true", help="no header (def = False)")
     args = vars(parser.parse_args())
     # print(args)
-
-    plotter = PandaPlotter(args)
-    plotter.do_plot()
+    PandaPlotter(args).do_plot()
 
